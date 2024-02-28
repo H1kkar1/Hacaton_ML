e83af401117d64125a4636ebaff997df4846c244
Rina <2011l11@mail.ru>
2024-02-28 14:36:47 +0300

    Более корректное чтение пдф

--- Case_1/main.py
+++ Case_1/main.py
@@ -2,14 +2,14 @@ from app.infrastacture.FileReader import FileReader
 
 
 def main():
-    text = FileReader("test.txt")
+    # text = FileReader("test.txt")
+    # print(text.Read())
+
+    text = FileReader("Vadim Brezovsky.pdf")
     print(text.Read())
 
-    text = FileReader("test.pdf")
-    print(text.Read())
-
-    text = FileReader("test.xlsx")
-    print(text.Read())
+    # text = FileReader("test.xlsx")
+    # print(text.Read())
 
 
 if __name__ == '__main__':
