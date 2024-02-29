81ce947753cafb4d80c87b55185a6b7e1007a036
Rina <2011l11@mail.ru>
2024-02-29 16:07:36 +0300

    fix

--- Case_1/main.py
+++ Case_1/main.py
@@ -1,10 +1,10 @@
 import json
 
 from app.core.JsonGenerator import JsonGenerator
-from app.core.Models.Person import Person
-from app.core.Models.Language import Language
-from app.core.Models.Experience import Experience
-from app.core.Models.Education import Education
+from app.core.Models.PersonItems import Person
+from app.core.Models.LanguageItems import Language
+from app.core.Models.ExperienceItems import Experience
+from app.core.Models.EducationItems import Education
 from app.infrastacture.FileReader import FileReader
 from app.core.Algorithms.PersinAlgorithm import PersonAlg
 from app.core.JsonGenerator import JsonGenerator
@@ -13,7 +13,7 @@ import json
 
 def main():
     text = FileReader("Nick Vinogradov.docx").Read()
-    person = PersonAlg(text).PersonCreate()
+    person = PersonAlg(text).person_create()
 
 
 
