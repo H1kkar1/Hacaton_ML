27b84c59a25c83fff9b4053c1e662f7613f786e8
H1kkar1 <kn_pavel@mail.ru>
2024-02-29 16:03:47 +0300

    фикс алгов

--- Case_1/main.py
+++ Case_1/main.py
@@ -1,20 +1,31 @@
 import json
 
 from app.core.JsonGenerator import JsonGenerator
-from app.core.Models.Person import Person
-from app.core.Models.Language import Language
-from app.core.Models.Experience import Experience
-from app.core.Models.Education import Education
+from app.core.Models.PersonItems import Person
+from app.core.Models.LanguageItems import Language
+from app.core.Models.ExperienceItems import Experience
+from app.core.Models.EducationItems import Education
 from app.infrastacture.FileReader import FileReader
 from app.core.Algorithms.PersinAlgorithm import PersonAlg
+from app.core.Algorithms.EducationAlgoritm import EducationAlg
+from app.core.Algorithms.ExperienceAlgoritm import ExperienceAlg
+from app.core.Algorithms.LanguageAlgoritm import LanguageAlg
 from app.core.JsonGenerator import JsonGenerator
 import json
 
 
 def main():
     text = FileReader("Nick Vinogradov.docx").Read()
-    person = PersonAlg(text).PersonCreate()
 
+    person = PersonAlg(text).person_create()
+    education = EducationAlg(text).education_create()
+    experience = ExperienceAlg(text).experience_create()
+    language = LanguageAlg(text).language_create()
+
+    generator = JsonGenerator(person, education, experience, language)
+    data = generator.Generate_json()
+    with open("result.json", "w") as f:
+        json.dump(data, f, indent=4)
 
 
 if __name__ == '__main__':
