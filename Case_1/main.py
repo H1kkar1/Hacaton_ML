bdb930274302524c8f2dffbed246c1f2fbaa9580
H1kkar1 <kn_pavel@mail.ru>
2024-02-29 14:40:07 +0300

    прбрался в коде

--- Case_1/main.py
+++ Case_1/main.py
@@ -7,11 +7,6 @@ import json
 def main():
     text = FileReader("Nick Vinogradov.docx").Read()
     person = PersonAlg(text).PersonCreate()
-    print(person.first_name)
-    print(person.last_name)
-    print(person.middle_name)
-    print(person.country)
-    print(person.contacts[0].value)
 
 
 
